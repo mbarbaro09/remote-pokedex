b2d63d590b0ec3d15fdc4e8be5c4090333ac556a
mbarbaro09 <marco.barbaro09@gmail.com>
2024-09-10 21:23:09 +0200

    Created and Initialized API project, .git and Unit test project

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace remote_pokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
